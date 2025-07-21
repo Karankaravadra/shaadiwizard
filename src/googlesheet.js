110bc4cae73cbd2391325e3be88ebaf12f5e083c
Karankaravadra <139636320+Karankaravadra@users.noreply.github.com>
2025-07-21 21:02:23 +0530

    Update googlesheet.js

--- src/googlesheet.js
+++ src/googlesheet.js
@@ -1,12 +1,12 @@
-export const sendToGoogleSheet = async (data) => {
+export const sendTogooglesheet = async (data) => {
   try {
-    console.log("Simulating sending data to Google Sheets:");
+    console.log("Simulating sending data to google sheets:");
     console.log("Data:", data);
     
     await new Promise((res) => setTimeout(res, 1000));
     return { success: true };
   } catch (error) {
-    console.error("Temporary GoogleSheet logger failed:", error);
+    console.error("Temporary googlesheet logger failed:", error);
     return { success: false, error };
   }
 };
